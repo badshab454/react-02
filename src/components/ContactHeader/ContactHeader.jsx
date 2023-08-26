3e8b25c1d52636830aac103cf65265d8162587e9
Badsha Hossain <badshab454@gmail.com>
2023-08-26 22:58:56 +0600

    component

--- src/components/ContactHeader/ContactHeader.jsx
+++ src/components/ContactHeader/ContactHeader.jsx
@@ -1,8 +1,20 @@
+import styles from "./ContactHeader.module.css";
 
 const ContactHeader = () => {
+  console.log(styles);
   return (
-    <div>ContactHeader</div>
-  )
-}
+    <div className="styles.contact">
+      <div className="contact-section">
+      <h1>contact us</h1>
+      <p>
+        LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU!
+        WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN
+        REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE,
+        EMAIL, OR SOCIAL MEDIA.
+      </p>
+      </div>
+    </div>
+  );
+};
 
 export default ContactHeader;
